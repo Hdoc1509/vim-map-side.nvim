8b87bdc51b5043b61582dc156dfdc81b3c453864
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-12-14 21:44:37 -0500

    fix(tree-sitter): use `extends` modeline to load optional queries
    related to https://github.com/neovim/neovim/issues/36959

--- lua/vim-map-side/tree-sitter/queries.lua
+++ lua/vim-map-side/tree-sitter/queries.lua
@@ -7,13 +7,13 @@ local function setup()
     -- see new features in treesiter section
     -- https://neovim.io/doc/user/news-0.11.html#_new-features
     -- https://neovim.io/doc/user/treesitter.html#vim.treesitter.query.set()
-    injection_query = "; inherits: modemap_fn\n"
+    -- NOTE: follow updates of https://github.com/neovim/neovim/issues/36959
+    -- injection_query = "; inherits: lua,vms_modemap_fn"
+    injection_query = "; extends\n"
   else
     -- reference:
     -- https://github.com/MeanderingProgrammer/render-markdown.nvim/blob/10126effbafb74541b69219711dfb2c631e7ebf8/lua/render-markdown/core/ts.lua#L56-L69
     local files = vim.treesitter.query.get_files("lua", "injections")
-    local modemap_query_path =
-      vim.treesitter.query.get_files("vms_modemap_fn", "injections")[1]
 
     for _, file in ipairs(files) do
       local f = assert(io.open(file, "r"))
@@ -22,14 +22,16 @@ local function setup()
       f:close()
       injection_query = injection_query .. body .. "\n"
     end
-
-    local modemap_query_file = assert(io.open(modemap_query_path, "r"))
-    local modemap_query = modemap_query_file:read("*a") --[[@as string]]
-
-    modemap_query_file:close()
-
-    injection_query = injection_query .. modemap_query
   end
+
+  local modemap_query_path =
+    vim.treesitter.query.get_files("vms_modemap_fn", "injections")[1]
+  local modemap_query_file = assert(io.open(modemap_query_path, "r"))
+
+  local modemap_query = modemap_query_file:read("*a") --[[@as string]]
+
+  modemap_query_file:close()
+  injection_query = injection_query .. modemap_query
 
   pcall(vim.treesitter.query.set, "lua", "injections", injection_query)
 end
